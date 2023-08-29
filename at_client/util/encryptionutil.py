6102da458e32099d1ac4ae5d6eb958aba6464c20
Xlin123 <xavier.li.1332@gmail.com>
2023-08-29 13:35:58 -0400

    fix: iv using secrets module (true random)

--- at_client/util/encryptionutil.py
+++ at_client/util/encryptionutil.py
@@ -2,6 +2,7 @@ import base64
 import binascii
 import os
 import random
+import secrets
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding, serialization
 from cryptography.hazmat.backends import default_backend
@@ -92,30 +93,5 @@ class EncryptionUtil:
     
     @staticmethod
     def generate_iv_nonce():
-        return IVNonce().token_bytes()
-
-class IVNonce:
-
-    def _randbytes(self, nbytes):
-        """Generate n random bytes."""
-        # CPython isn't limited to 32 bits:
-        # return random.getrandbits(nbytes * 8).to_bytes(n, 'little')
+        return secrets.token_bytes(16)
         
-        randbytes = bytearray()
-        while nbytes > 0:
-            num_bytes = min(nbytes, 4)  # Generation is limited to 32 bits (4 bytes)
-            randbits = random.getrandbits(num_bytes * 8)
-            randbytes.extend(randbits.to_bytes(num_bytes, 'little'))
-            nbytes -= num_bytes
-        return bytes(randbytes)
-    
-    def token_bytes(self):
-        return self.randbytes
-    
-    def token_hex(self):
-        # check upython compat for decode
-        return binascii.hexlify(self.token_bytes()).decode('ascii') 
-
-    def __init__(self, n:int=16):
-        self.n = n
-        self.randbytes = self._randbytes(n)
