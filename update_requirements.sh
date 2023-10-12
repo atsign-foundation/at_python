a4de5affed8115943dea02218bf59c6376e2d585
Chris Swan <478926+cpswan@users.noreply.github.com>
2023-10-12 13:56:53 +0100

    ci: Make use of dev dependencies for CI

--- update_requirements.sh
+++ update_requirements.sh
@@ -4,6 +4,7 @@
 # in our Python dependencies. So we need to run this to generate a
 # new requirements.txt from the updated files.
 poetry export --format requirements.txt --output requirements.txt
+poetry export --format requirements.txt --output requirements.dev --with dev
 # of course this assumes that poetry is installed, which is done by:
 ## curl -sSL https://install.python-poetry.org | python3 -
 # see https://github.com/atsign-foundation/at_server/pull/1065 for
